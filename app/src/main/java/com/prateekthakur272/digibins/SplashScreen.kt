3b6923e34ddfe7e571229597af83c59ed58499c6
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2022-11-04 20:41:18 +0530

    Splash Screen created and app icon created

---
+++ app/src/main/java/com/prateekthakur272/digibins/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.prateekthakur272.digibins
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },2000)
+    }
+}
